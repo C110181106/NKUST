08d7903d043691bcf9b314edb8fe53f1000da577
C110181106 <c110181106@nkust.edu.tw>
2021-11-16 14:32:54 +0800

    This is my first commit

--- BMI.java
+++ BMI.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BMI {
+public class BMI {
+    public static void main(String[] args) {
+        int weight=73;
+        double height=18.1;
+        double BMI;
+        BMI = weight/(height*height);
+        System.out.println("Your BMI:"+BMI);
+    }
 }
